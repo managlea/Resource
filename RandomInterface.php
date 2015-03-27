589331d7ad5482f7c9012138ba8c3a6de99d0160
kaurikk <kaurikk@users.noreply.github.com>
2015-03-27 15:54:05 +0200

    Update RandomInterface.php

--- RandomInterface.php
+++ RandomInterface.php
@@ -3,10 +3,5 @@
 namespace Managlea\Randon;
 
 interface ResourceManagerInterface {
-	
 	public static function getSingle($resourceName, $resourceId);
-	
-	//public static function getCollection($resourceName, array $filters = array());
-	
-	//public static function postSingle($resourceName, array $data);
 }
